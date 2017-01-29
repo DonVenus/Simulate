a27fe340d925e7350b35dfcd106011363a42abf8
Donald Venus <donaldmvenus@gmail.com>
2017-01-29 01:12:23 -0500

    Basic combat and health implemented

--- game.js
+++ game.js
@@ -16,23 +16,45 @@
 		}
 		, false);
 
-	var personArray = [];
+	var redArray = [];
+	var blueArray = [];
+	var laserArray = [];
 	function person(positionX, positionY, color) {
 		this.posX = positionX;
 		this.posY = positionY;
 		this.indColor = color;
+		this.coolDown = 0;
+		this.health = 3;
+		this.fireLaser = function(velX, velY) {
+			laserArray.push(new laser(this.posX, this.posY, velX, velY, this.indColor));
+			this.coolDown = 20;
+		}
+	}
+
+	function laser(posX, posY, velX, velY, color) {
+		this.posX = posX + velX;
+		this.posY = posY + velY;
+		this.velX = velX;
+		this.velY = velY;
+		this.indColor = color;
+		this.move = function() {
+			this.posX += velX;
+			this.posY += velY;
+		}
 	}
 
 
 	var blueCounter = document.getElementById('blue-count');
 	var redCounter = document.getElementById('red-count');
 	function handleClick(x, y) {
-		personArray.push(new person(x, y, placeColor));
+		var per = new person(x, y, placeColor)
 		if (placeColor == redColor) {
+			redArray.push(per);
 			redCount++;
 			redCounter.innerHTML = redCount;
 		}
 		if (placeColor == blueColor) {
+			blueArray.push(per);
 			blueCount++;
 			blueCounter.innerHTML = blueCount;
 		}
@@ -54,11 +76,28 @@
 		window.requestAnimationFrame(runGame);
 	}
 
-	function runGame() {
-		ctx.clearRect(0,0,600,600);
+	function findDist(a, b) {
+		return Math.sqrt((Math.pow((a.posX - b.posX),2) + Math.pow((a.posY - b.posY),2)));
+	}
+
+	function handlePeople(personArray) {
 		for (var i = 0; i < personArray.length; i++) {
 			ctx.fillStyle = personArray[i].indColor;
-			ctx.fillRect(personArray[i].posX, personArray[i].posY, 4, 4);
+			ctx.fillRect(personArray[i].posX, personArray[i].posY, 5, 5);
+			if (personArray[i].health == 2) {
+				ctx.fillStyle = 'rgba(255, 255, 255, 0.33';
+				ctx.fillRect(personArray[i].posX, personArray[i].posY, 5, 5);
+			}
+			if (personArray[i].health == 1) {
+				ctx.fillStyle = 'rgba(255, 255, 255, 0.66';
+				ctx.fillRect(personArray[i].posX, personArray[i].posY, 5, 5);
+			}
+			if (personArray[i].health == 0) {
+				ctx.fillStyle = 'rgba(255, 255, 255, 1';
+				ctx.fillRect(personArray[i].posX, personArray[i].posY, 5, 5);
+				personArray.splice(i, 1);
+				continue;
+			}
 			var moveX = Math.floor(Math.random() * 2);
 			var moveY = Math.floor(Math.random() * 2);
 			var moveUp = Math.floor(Math.random() * 2);
@@ -91,8 +130,74 @@
 					personArray[i].posY--;
 				}
 			}
+			if (personArray[i].indColor == blueColor) {
+				findTarget(personArray[i], redArray);
+			}
+			if (personArray[i].indColor == redColor) {
+				findTarget(personArray[i], blueArray);
+			}
 		}
+	}
 
+	function findTarget(person, arr) {
+		if (person.coolDown == 0) {
+			if (arr.length > 0) {
+				var closest = arr[0];
+				var closestDist = findDist(person, arr[0]);
+				for (var i = 1; i < arr.length; i++) {
+					var dist = findDist(person, arr[i]);
+					if (dist < closestDist) {
+						closest = arr[i];
+						closestDist = dist;
+					}
+				}
+				var errX = Math.floor((Math.random() * closestDist / 8) - closestDist / 16);
+				var errY = Math.floor((Math.random() * closestDist / 8) - closestDist / 16);
+				var diffX = closest.posX - person.posX + errX;
+				var diffY = closest.posY - person.posY + errY;
+				var angleRad = Math.atan2(diffY, diffX);
+				var xVel = Math.cos(angleRad) * 5;
+				var yVel = Math.sin(angleRad) * 5;
+				person.fireLaser(xVel, yVel);
+			}
+		}
+		else {
+			person.coolDown--;
+		}	
+	}
+
+	function handleLasers() {
+		for (var j = 0; j < laserArray.length; j++) {
+			ctx.fillStyle = laserArray[j].indColor;
+			ctx.fillRect(laserArray[j].posX, laserArray[j].posY, 3, 3);
+			laserArray[j].move();
+			if (laserArray[j].posX > 600 || laserArray[j].posX < 0 || laserArray[j].posY > 600 || laserArray[j].posY < 0) {
+				laserArray.splice(j, 1);
+			}
+			else {
+				if(!handleLaserCollisions(laserArray[j], j, redArray)) {
+					handleLaserCollisions(laserArray[j], j, blueArray);
+				}
+			}
+		}
+	}
+
+	function handleLaserCollisions(laser, index, arr) {
+		for (var i = 0; i < arr.length; i++) {
+			if (Math.abs(arr[i].posX - laser.posX) <= 4 && Math.abs(arr[i].posY - laser.posY) <= 4) {
+				laserArray.splice(index, 1);
+				arr[i].health--;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function runGame() {
+		ctx.clearRect(0,0,600,600);
+		handleLasers();
+		handlePeople(redArray);
+		handlePeople(blueArray);
 		window.requestAnimationFrame(runGame);
 	}
 
